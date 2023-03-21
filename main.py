3b0b29dbb95624ded1ce49f61dc055ae3a7bc067
TouficAlRifaii <toufic.alrifaii@lau.edu>
2023-03-21 14:35:51 +0200

    Creating a method to send get requets

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-import requests 
+import requests
+
+def request(url):
+    try:
+        return requests.get("http://" + url)
+    except requests.exceptions.ConnectionError:
+        try:
+            return requests.get("https://" + url)
+        except requests.exceptions.ConnectionError:
+            pass
+
+url  = "testphp.vulnweb.com/"
+
+print(request(url))

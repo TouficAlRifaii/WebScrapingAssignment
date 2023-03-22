8e3e3cd628a7b535b067b48e34738eec8846bd27
TouficAlRifaii <toufic.alrifaii@lau.edu>
2023-03-22 13:00:20 +0200

    Completing the getSubdomains method

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 
+
 def request(url):
     try:
         return requests.get("http://" + url)
@@ -9,6 +10,20 @@ def request(url):
         except requests.exceptions.ConnectionError:
             pass
 
-url  = "testphp.vulnweb.com/"
 
-print(request(url))
+def getSubdomains(url):
+    with open("subdomains_dictionary.bat") as file:
+        output_file = open("subdomains_output.bat", "w")
+        for line in file:
+            word = line.strip()
+            target = word + "." + url
+            response = request(target)
+            if response:
+                print(target + "\n")
+                output_file.write(target + "\n")
+        output_file.close()
+
+
+url = "google.com"
+
+print(getSubdomains(url))
